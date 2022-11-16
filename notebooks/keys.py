53497b8751b28bbc7711c122f54c10bd9c246639
nelealyssa <115080867+nelealyssa@users.noreply.github.com>
2022-11-16 16:48:28 +0100

    Keys & erster Twitter Code

---
+++ notebooks/keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='tQ87kCwL6jTbmhCPgpfuIy98Z'
+
+consumer_secret='ifM1jNvTW31onofnZzUjwIUAya5N9byxc4VOI1vSwSQei68WF8t'
+
+access_token='1582678895706492928-yfKfeboybVfIlD8iJmlymNcG6HIFNR'
+
+access_token_secret='Sai84wboH56uAgbkPkiD24A2swSNwbOErk2XSSdLSMgJY'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAJtgiQEAAAAAzSquDACrPTOsjy17BONUp0bKBHg%3Dwy7fPdozrPxDTzaSoAyebOG31Z7cbwrsJeGqO8kLZrQQKC8gaJ'
