482cf4c086c7b0dec42d2f796b7d37d4d88e8f6e
timstarz <115080857+timstarz@users.noreply.github.com>
2022-12-18 20:55:28 +0100

    Aufgabe 4: Korrekturen, Aufgabe 5: komplett (Pie Chart wird nicht angezeigt - nervig)

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,59 +4,88 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+from pathlib import Path
 
-#-------------------
-#-------------------#
+home_path = str(Path.home())
+
 # IMPORT DATA
 
-df = pd.read_csv('/Users/timstarz/Documents/Wintersemester 22 23/Big Data/Hausi/homework-1/data/external/weather-check.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/nelealyssa/homework-1/main/data/external/data.csv')
 
 
-# Data preparation
-df = df.sort_values(by=['gdp_per_capita'], ascending=False)
-
-###-------------------###
-# START OF OUR APP
-
-#-------------------#
 # HEADER
 st.header('Group L: Dashboard')
 
 # Title of our app
 st.title("Task 5: Streamlit")
 
+st.dataframe(df)
+
+
 # BODY
-
-# Show df and highlight some attributes
-st.write("DataFrame with highlight_max:")
-st.dataframe(df.style.highlight_max(axis=0))
-
-
-# Bar chart
-st.subheader("Bar chart")
+st.write("Visualisierung 1: Bar Chart Altair")
 
 c = alt.Chart(df).mark_bar().encode(
-    x='country',
-    y='life_satisfaction',
-    tooltip = ['country', "life_satisfaction"]
+        x=alt.X('What is your gender?',
+               sort='-y'), 
+        y=alt.Y('count(What is your gender?)')
 )
+st.altair_chart(c, use_container_width=True)
+
+
+st.write("Visualisierung 2: Dodged Bar Chart Altair")
+
+c = alt.Chart(df).mark_bar().encode(
+    x=alt.X('Age', 
+            sort = '-y', 
+            axis=alt.Axis(titleAnchor="start", 
+                          labelAngle=0)),
+    y=alt.Y('count(Age)', 
+            axis=alt.Axis(title = "Count", 
+                          titleAnchor="start")),
+    color=alt.Color('Do you typically check a daily weather report?', 
+                    sort="descending", 
+                    legend=alt.Legend(title="Type"))
+).properties(
+    title='Do you typically check a daily weather report?',
+    width=500,
+    height=350
+).configure_title(
+    fontSize=16,
+    font='Arial',
+    anchor='start',
+)
+st.altair_chart(c, use_container_width=True)
+
+
+st.write("Visualisierung 3: Scatterplot Paired Data Altair (interactive)")
+
+c = alt.Chart(df).mark_circle(size=70).encode( 
+    x=alt.X('US Region', 
+            title='US Region'),
+    y=alt.Y('Age', 
+            title='Age'),
+    tooltip=['US Region', 'Age'],
+    color= alt.Color('count(US Region)',
+                     legend=alt.Legend(title="count"))
+).interactive()
 
 st.altair_chart(c, use_container_width=True)
 
-# Show metric
-st.subheader("Display Metrics")
-st.metric("My metrics", 32, 4)
 
-# Add slider with user input
-st.subheader("Slider")
-st.write("This is a Slider")
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+st.write("Visualisierung 4: Pie Chart Altair")
 
-# Show code
-st.subheader("Show some code")
+c = chart = alt.Chart(source).encode(
+    theta=alt.Theta("value:Q", stack=True), 
+    color=alt.Color("category:N", legend=None)
+).properties(
+    title='If you had a smartwatch (like the soon to be released Apple Watch), how likely or unlikely would you be to check the weather on that device?',
+    width=500,
+    height=350
+)
+pie = chart.mark_arc(outerRadius=130)
+text = chart.mark_text(radius=165, size=10).encode(text="category:N")
 
-code = '''def hello():
-     print("Hello, Streamlit!")'''
+pie + text 
 
-st.code(code, language='python')
+st.altair_chart(c, use_container_width=True)
